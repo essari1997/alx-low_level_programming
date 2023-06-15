d22b5b3988efffe36d90efd497b33722123d5ded
essari1997 <essari.aiac@gmail.com>
2023-06-15 16:49:26 -0700

    .

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("Size of a char : %1u byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int : %1u byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int : %1u bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int : %1u bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float : %1u bytes(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
