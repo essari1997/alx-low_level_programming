5804763fa4f05cbfef205709c26e44cbd61e625a
essari1997 <essari.aiac@gmail.com>
2023-06-20 17:38:16 -0700

    nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,12 @@
-#include "main.h"
+#include 'main.h'
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int main(void)
 {
 	_putchar('_');
