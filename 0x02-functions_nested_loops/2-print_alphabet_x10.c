12fef505107c7841f1871a459502470d228fdfef
essari1997 <essari.aiac@gmail.com>
2023-06-20 19:06:25 -0700

    nested

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,16 @@
 
 /**
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
- */A
+ */
 void print_alphabet_x10(void)
 {
-	int i, j;
+	int count = 0;
+	char letter;
 
-	for (i = 0; i < 10; i++)
+	while (count++ <= 9)
 	{
-		for (j = 'a'; j <= 'z'; j++)
-			_putchar(j);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
 	}
 }
