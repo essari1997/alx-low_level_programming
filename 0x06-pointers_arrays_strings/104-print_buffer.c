178fa642288f9a768a872611832d24336d3c3008
essari1997 <essari.aiac@gmail.com>
2023-07-02 11:21:29 -0700

    more pointers

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,28 +10,33 @@ void print_buffer(char *b, int size)
 {
 	int i, j, l;
 
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%.8x:", i);
-		for (j = 0; j < 10; j++)
-		{
-			if ((j + i) % 2 == 0)
-				printf(" ");
-			if (j + i < size)
-				printf("%.2x", *(b + i + j));
-			else
-				printf("  ");
-		}
-		printf(" ");
-		for (l = i; l < i + 10; l++)
-		{
-			if (l >= size)
-				break;
-			if (*(b + l) < 32 || *(b + l) > 126)
-				printf("%c", '.');
-			else
-				printf("%c", *(b + l));
-		}
+	if (size <= 0)
 		printf("\n");
+	else
+	{
+		for (i = 0; i < size; i += 10)
+		{
+			printf("%.8x:", i);
+			for (j = 0; j < 10; j++)
+			{
+				if ((j + i) % 2 == 0)
+					printf(" ");
+				if (j + i < size)
+					printf("%.2x", *(b + i + j));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = i; l < i + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
 	}
 }
